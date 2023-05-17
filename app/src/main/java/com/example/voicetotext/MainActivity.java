3a772e5b6ef7dac87cea922811db37eacb88951d
HongJongYeon <yjgno2@naver.com>
2023-05-17 11:16:00 +0900

    1

--- app/src/main/java/com/example/voicetotext/MainActivity.java
+++ app/src/main/java/com/example/voicetotext/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.voicetotext;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn;
@@ -21,8 +29,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn = (Button) findViewById(R.id.btn);
         tv = (TextView) findViewById(R.id.tv);
         btn.setOnClickListener(this);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(recognitionListener);
     }
+
+    @Override
+    public void onClick(View view) {
+        btn.setText("말 하는 중....");
+        mRecognizer.startListening(intent);
+    }
+    private RecognitionListener recognitionListener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            btn.setText("음성인식 대기중....");
+            tv.setText("");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            btn.setText("버튼을 누르고 말하세요");
+
+        }
+
+        @Override
+        public void onError(int i) {
+            tv.setText("말이 없네요");
+            btn.setText("버튼을 누르고 말하세요");
+
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            ArrayList<String> mResult = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            tv.setText(rs[0]);
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+    };
+
+
+}
 
